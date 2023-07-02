6585f0beac208df66d6296432dfbdb866955e90b
SaiKrupaReddy20 <cs16b012@iittp.ac.in>
2023-07-02 18:24:47 +0530

    backend_url_added

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const databaseUrl ='mongodb+srv://saikrupa1320:fHXYsVqsBpowCqVO@cluster0.irifunz.mongodb.net/book-list-app';
+const databaseUrl =process.env.MONGODB_URL;
 const dbConnect = () => {
     mongoose
       .connect(databaseUrl, {})
